dae6f99e3398c8c11b1ce9928bca071ef58fb690
ᏒᎧᏂᎥᏖ ᏗᏰᎴᏬᏝᏝᏗᏂ <74206938+rohitabdullah@users.noreply.github.com>
2023-11-02 02:21:49 -0500

    Update PNP.py

--- PNP.py
+++ PNP.py
@@ -1,15 +1,6 @@
 import sys
-
-def main():
-
-  with open("banner.txt", "r") as banner_file:
-    banner_text = banner_file.read()
-
-  print(banner_text)
-
 import requests
 import json
-
 
 def is_public_uid(uid):
   """Checks if a Facebook UID is public.
@@ -25,41 +16,49 @@ def is_public_uid(uid):
   response = requests.get(
       "https://graph.facebook.com/v13.0/{}/fields=is_public".format(uid))
 
- 
+
   if response.status_code == 200:
-    
+
     json_response = response.json()
 
-    
     is_public = json_response["is_public"]
 
     return is_public
   else:
-    
+
     return False
 
+def main():
 
-text_file_path = input("Enter the path to the text file: ")
-
-
-public_uids = []
-
-
-with open(text_file_path, "r") as text_file:
-  for line in text_file:
-    uid = line.strip()
-
-    
-    is_public = is_public_uid(uid)
-
-    
-    if is_public:
-      public_uids.append(uid)
+  with open("banner.txt", "r") as banner_file:
+    banner_text = banner_file.read()
 
 
-with open("public_uids.txt", "w") as public_uids_file:
-  for uid in public_uids:
-    public_uids_file.write(uid + "\n")
+  print(banner_text)
 
 
-print("The public UIDs have been saved to the file 'public_uids.txt'.")
+  text_file_path = input("Where is the file?: ")
+
+  destination_file_path = input("Where to save the file?: ")
+
+
+  public_uids = []
+
+
+  with open(text_file_path, "r") as text_file:
+    for line in text_file:
+      uid = line.strip()
+
+      is_public = is_public_uid(uid)
+
+      if is_public:
+        public_uids.append(uid)
+
+  with open(destination_file_path, "w") as destination_file:
+    for uid in public_uids:
+      destination_file.write(uid + "\n")
+
+  print("The public UIDs have been saved to the file '{}'.".format(destination_file_path))
+
+if __name__ == "__main__":
+  main()
