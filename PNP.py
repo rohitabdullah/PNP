8a99a5f5f169b55355afdaa0a59f6674c7137179
ᏒᎧᏂᎥᏖ ᏗᏰᎴᏬᏝᏝᏗᏂ <74206938+rohitabdullah@users.noreply.github.com>
2023-11-02 01:23:10 -0500

    Update PNP.py

--- PNP.py
+++ PNP.py
@@ -1,5 +1,15 @@
+import sys
+
+def main():
+
+  with open("banner.txt", "r") as banner_file:
+    banner_text = banner_file.read()
+
+  print(banner_text)
+
 import requests
 import json
+
 
 def is_public_uid(uid):
   """Checks if a Facebook UID is public.
@@ -11,48 +21,45 @@ def is_public_uid(uid):
     True if the Facebook UID is public, False otherwise.
   """
 
-  # Make a GET request to the Facebook Graph API.
+
   response = requests.get(
       "https://graph.facebook.com/v13.0/{}/fields=is_public".format(uid))
 
-  # Check the response status code.
+ 
   if response.status_code == 200:
-    # The response is successful.
+    
     json_response = response.json()
 
-    # Check the is_public field.
+    
     is_public = json_response["is_public"]
 
     return is_public
   else:
-    # The response was not successful.
+    
     return False
 
-# Get the path to the text file.
+
 text_file_path = input("Enter the path to the text file: ")
 
-# Get the name of the output file.
-output_file_name = input("Enter the name of the output file: ")
 
-# Create a list to store the public UIDs.
 public_uids = []
 
-# Read the text file line by line.
+
 with open(text_file_path, "r") as text_file:
   for line in text_file:
     uid = line.strip()
 
-    # Check if the UID is public.
+    
     is_public = is_public_uid(uid)
 
-    # If the UID is public, add it to the list.
+    
     if is_public:
       public_uids.append(uid)
 
-# Write the public UIDs to the output file.
-with open(output_file_name, "w") as output_file:
-  for uid in public_uids:
-    output_file.write(uid + "\n")
 
-# Print a message to the user.
-print(f"The public UIDs have been saved to the file '{output_file_name}'.")
+with open("public_uids.txt", "w") as public_uids_file:
+  for uid in public_uids:
+    public_uids_file.write(uid + "\n")
+
+
+print("The public UIDs have been saved to the file 'public_uids.txt'.")
