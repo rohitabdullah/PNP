65e9c0978eccce211919ce2c79ab43cf211dd4d4
ᏒᎧᏂᎥᏖ ᏗᏰᎴᏬᏝᏝᏗᏂ <74206938+rohitabdullah@users.noreply.github.com>
2023-11-03 04:43:22 -0500

    Update PNP.py

--- PNP.py
+++ PNP.py
@@ -1,15 +1,13 @@
 import os
 import requests
-import json
-import tqdm
-import os
+import textwrap
 import shutil
 import webbrowser
-import textwrap
+import tqdm
 
-def is_access_token_active():
+def is_access_token_active(access_token):
     response = requests.get(
-        "https://graph.facebook.com/v13.0/me"
+        f"https://graph.facebook.com/v13.0/me?access_token={access_token}"
     )
     json_response = response.json()
 
@@ -18,70 +16,69 @@ def is_access_token_active():
     else:
         return True
 
-# Get the banner text from the bs.txt file in the repository.
-banner_text = requests.get("https://raw.githubusercontent.com/rohitabdullah/pnp/main/bs.txt").text
+# Function to check if a UID is public. You need to implement the actual logic.
+def is_public_uid(uid, access_token):
+    # Replace this with the actual logic to check if a UID is public.
+    pass
 
-# Calculate the center of the terminal.
-terminal_width = shutil.get_terminal_size()[0]
-terminal_height = shutil.get_terminal_size()[1]
-center_x = terminal_width // 2
-center_y = terminal_height // 2
+def main():
+    # Get the banner text from the "bs.txt" file.
+    with open("bs.txt", "r") as banner_file:
+        banner_text = banner_file.read()
 
-# Wrap the banner text to fit the terminal width.
-banner_text_wrapped = textwrap.fill(banner_text, terminal_width)
+    # Calculate the center of the terminal.
+    terminal_width = shutil.get_terminal_size().columns
+    center_x = terminal_width // 2
 
-# Print the banner text centered in the terminal.
-for line in banner_text_wrapped.splitlines():
-    print(f"{' ' * center_x}{line}")
+    # Wrap the banner text to fit the terminal width.
+    banner_text_wrapped = textwrap.fill(banner_text, terminal_width)
 
-# Ask the user if they have an access token.
-has_access_token = input("Do you have an access token? (Y/N): ")
+    # Print the banner text centered in the terminal.
+    for line in banner_text_wrapped.splitlines():
+        print(f"{' ' * center_x}{line}")
 
-if has_access_token.lower() == "y":
+    # Ask the user for their access token.
+    access_token = input("Enter your access token: ")
 
     # Check if the access token is active.
-    if not is_access_token_active():
-        print("Your access token is not active. Please generate a new access token at https://developers.facebook.com/tools/explorer/.")
+    if not is_access_token_active(access_token):
+        print("Your access token is not active. Please generate a new access token at https://developers.facebook.com/tools/explorer/")
         exit()
 
-else:
+    # Prompt the user for the path to the text file containing UIDs.
+    text_file_path = input("Enter the path to the text file: ")
 
-    # Provide the user with a link to generate an access token.
-    print("Please generate an access token at https://developers.facebook.com/tools/explorer/.")
-    exit()
+    # Prompt the user for the path to save the public UIDs.
+    public_uids_file_path = input("Enter the path where to save the public UIDs: ")
 
-# Get the path to the text file containing the UIDs.
-text_file_path = input("Enter the path to the text file: ")
+    # Create a list to store the public UIDs.
+    public_uids = []
 
-# Get the path where to save the public UIDs.
-public_uids_file_path = input("Enter the path where to save the public UIDs: ")
+    # Iterate over the lines in the text file and check if each UID is public.
+    with open(text_file_path, "r") as text_file:
+        for line in tqdm.tqdm(text_file):
+            uid = line.strip()
+            is_public = is_public_uid(uid, access_token)  # You need to implement this function.
 
-# Create a list to store the public UIDs.
-public_uids = []
+            if is_public:
+                public_uids.append(uid)
 
-# Iterate over the lines in the text file and check if each UID is public.
-with open(text_file_path, "r") as text_file:
-    for line in tqdm.tqdm(text_file):
-        uid = line.strip()
-        is_public = is_public_uid()
+    # Save the public UIDs to a file.
+    with open(public_uids_file_path, "w") as public_uids_file:
+        for uid in public_uids:
+            public_uids_file.write(uid + "\n")
 
-        if is_public:
-            public_uids.append(uid)
+    # Inform the user that the public UIDs have been saved.
+    print(f"The public UIDs have been saved to the file '{public_uids_file_path}'.")
 
-# Save the public UIDs to a file.
-with open(public_uids_file_path, "w") as public_uids_file:
-    for uid in public_uids:
-        public_uids_file.write(uid + "\n")
+    # Ask the user if they want to follow the author on GitHub.
+    follow_on_github = input("Do you want to follow the author on GitHub? (Y/N): ")
 
-# Inform the user that the public UIDs have been saved.
-print("The public UIDs have been saved to the file '{}'.".format(public_uids_file_path))
+    if follow_on_github.lower() == "y":
+        webbrowser.open("https://github.com/rohitabdullah")
+        print("Thanks for following the author on GitHub!")
+    else:
+        print("Thank you for using this tool!")
 
-# Ask the user if they want to follow you on GitHub.
-follow_on_github = input("Do you want to follow me on GitHub? (Y/N): ")
-
-if follow_on_github.lower() == "y":
-    os.system("open https://github.com/rohitabdullah")
-    print("Thanks for following me on GitHub!")
-else:
-    print("Thank you for using this tool!")
-
+if __name__ == "__main__":
+    main()
