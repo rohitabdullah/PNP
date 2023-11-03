e1b461fe454aad620b8d92dbca2145b913bc101d
ᏒᎧᏂᎥᏖ ᏗᏰᎴᏬᏝᏝᏗᏂ <74206938+rohitabdullah@users.noreply.github.com>
2023-11-03 02:24:56 -0500

    Update PNP.py

--- PNP.py
+++ PNP.py
@@ -1,9 +1,11 @@
-import PIL.Image
+import os
 import requests
 import json
 import tqdm
 import os
 import shutil
+import webbrowser
+import textwrap
 
 def is_public_uid(uid, access_token):
     response = requests.get(
@@ -13,26 +15,42 @@ def is_public_uid(uid, access_token):
     json_response = response.json()
     return json_response["is_public"] if "is_public" in json_response else False
 
-def get_image_from_url(image_url):
-    response = requests.get(image_url)
-    return PIL.Image.open(response.content)
+# Get the banner text from the bs.txt file in the repository.
+banner_text = requests.get("https://raw.githubusercontent.com/rohitabdullah/pnp/main/bs.txt").text
 
-# Display the banner text file.
-banner_text_file = open("bs.txt", "r")
-banner_text = banner_text_file.read()
-print(banner_text)
-banner_text_file.close()
+# Calculate the center of the terminal.
+terminal_width = shutil.get_terminal_size()[0]
+terminal_height = shutil.get_terminal_size()[1]
+center_x = terminal_width // 2
+center_y = terminal_height // 2
 
-# Prompt the user to enter their access token.
-access_token = input("Enter your Facebook access token: ")
+# Wrap the banner text to fit the terminal width.
+banner_text_wrapped = textwrap.fill(banner_text, terminal_width)
 
-# Check if the access token is active.
-if not is_access_token_active(access_token):
-    print("Your access token is not active. Please check your access token at https://developers.facebook.com/tools/explorer/.")
-    print("To generate a new access token, please visit https://developers.facebook.com/apps/.")
+# Print the banner text centered in the terminal.
+for line in banner_text_wrapped.splitlines():
+    print(f"{' ' * center_x}{line}")
+
+# Ask the user if they have an access token.
+has_access_token = input("Do you have an access token? (Y/N): ")
+
+if has_access_token.lower() == "y":
+
+    # Prompt the user for their access token.
+    access_token = input("Enter your access token: ")
+
+    # Check if the access token is active.
+    if not is_access_token_active(access_token):
+        print("Your access token is not active. Please generate a new access token at https://developers.facebook.com/tools/explorer/.")
+        exit()
+
+else:
+
+    # Provide the user with a link to generate an access token.
+    print("Please generate an access token at https://developers.facebook.com/tools/explorer/.")
     exit()
 
-# Get the path to the text file.
+# Get the path to the text file containing the UIDs.
 text_file_path = input("Enter the path to the text file: ")
 
 # Get the path where to save the public UIDs.
@@ -42,32 +60,27 @@ public_uids_file_path = input("Enter the path where to save the public UIDs: ")
 public_uids = []
 
 # Iterate over the lines in the text file and check if each UID is public.
-with open(text_file_path) as text_file:
-    for line in text_file:
+with open(text_file_path, "r") as text_file:
+    for line in tqdm.tqdm(text_file):
         uid = line.strip()
         is_public = is_public_uid(uid, access_token)
 
         if is_public:
             public_uids.append(uid)
-        else:
-            print(f"{uid} is not public.")
 
 # Save the public UIDs to a file.
 with open(public_uids_file_path, "w") as public_uids_file:
     for uid in public_uids:
         public_uids_file.write(uid + "\n")
 
-# If there are no public UIDs, print a message to the user.
-if not public_uids:
-    print("Sorry man!! None of the uids was public.. try another file")
-else:
-    print(f"The public UIDs have been saved to the file '{public_uids_file_path}'.")
+# Inform the user that the public UIDs have been saved.
+print("The public UIDs have been saved to the file '{}'.".format(public_uids_file_path))
 
-# Ask the user if they want to follow me on GitHub.
+# Ask the user if they want to follow you on GitHub.
 follow_on_github = input("Do you want to follow me on GitHub? (Y/N): ")
 
 if follow_on_github.lower() == "y":
     webbrowser.open("https://github.com/rohitabdullah")
     print("Thanks for following me on GitHub!")
 else:
-    print("No worries. Thanks for using my tool!")
+    print("Thank you for using this tool!")
