1ca0d1f66d547c4cf1e3a0f2acc862be65e2d5cf
ᏒᎧᏂᎥᏖ ᏗᏰᎴᏬᏝᏝᏗᏂ <74206938+rohitabdullah@users.noreply.github.com>
2023-11-02 20:46:13 -0500

    Update PNP.py

--- PNP.py
+++ PNP.py
@@ -3,6 +3,24 @@ import json
 import tqdm
 import os
 
+#logo 
+logo=(""
+`7MM"""Mq.`7MN.   `7MF'`7MM"""Mq. 
+  MM   `MM. MMN.    M    MM   `MM.
+  MM   ,M9  M YMb   M    MM   ,M9 
+  MMmmdM9   M  `MN. M    MMmmdM9  
+  MM        M   `MM.M    MM       
+  MM        M     YMM    MM       
+.JMML.    .JML.    YM  .JMML."")
+
+def clear():
+  os.system('clear')
+print(logo)
+print(42*'-')
+print("▌│█║▌║▌║ 𝚁.𝙰 𝙶𝚛𝚘𝚞𝚙. ║▌║▌║█│▌")
+print(42*'-')
+
+#uid check
 def is_public_uid(uid):
   response = requests.get(
       "https://graph.facebook.com/v13.0/{}/fields=is_public".format(uid))
@@ -10,46 +28,39 @@ def is_public_uid(uid):
   is_public = json_response["is_public"]
   return is_public
 
-
+# Clear the console.
 import os
 os.system('cls' if os.name == 'nt' else 'clear')
 
+# Get the path to the text file.
+text_file_path = input("Enter the path to the text file: ")
 
-with open("banner.txt", "r") as banner_file:
-  banner_text = banner_file.read()
+# Get the path where to save the public UIDs.
+public_uids_file_path = input("Enter the path where to save the public UIDs: ")
 
-
-print(banner_text)
-
-
-text_file_path = input("Where is the text file (path): ")
-
-
-public_uids_file_path = input("Where to save the public UIDs (path): ")
-
-
+# Create a list to store the public UIDs.
 public_uids = []
 
-
+# Iterate over the lines in the text file and check if each UID is public.
 with open(text_file_path) as text_file:
   for line in text_file:
     uid = line.strip()
     is_public = is_public_uid(uid)
 
-
+    # Print the UID and indicate whether it is public or not public.
     if is_public:
       print(f"{uid} is public.")
       public_uids.append(uid)
     else:
       print(f"{uid} is not public.")
 
-
+# Save the public UIDs to a file.
 with open(public_uids_file_path, "w") as public_uids_file:
   for uid in public_uids:
     public_uids_file.write(uid + "\n")
 
-
+# If there are no public UIDs, print a message to the user.
 if len(public_uids) == 0:
-  print("Sorry man!! None of the UIDs were public.. Try another file")
+  print("Sorry man!! None of the uids was public.. try another file")
 else:
   print("The public UIDs have been saved to the file '{}'.".format(public_uids_file_path))
