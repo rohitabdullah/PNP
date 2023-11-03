9799501605f0230876f5fc9e93defb925934916e
ᏒᎧᏂᎥᏖ ᏗᏰᎴᏬᏝᏝᏗᏂ <74206938+rohitabdullah@users.noreply.github.com>
2023-11-02 22:24:40 -0500

    Update PNP.py

--- PNP.py
+++ PNP.py
@@ -1,3 +1,4 @@
+import webbrowser
 import requests
 import json
 import tqdm
@@ -10,46 +11,49 @@ def is_public_uid(uid):
   is_public = json_response["is_public"]
   return is_public
 
-# Clear the console.
+#  GitHub profile
+webbrowser.open("https://github.com/rohitabdullah")
+
+# Clear 
 import os
 os.system('cls' if os.name == 'nt' else 'clear')
 
-# Read the banner
-banner_file_path = "bs.txt"
+# Read the banner.
+banner_file_path = "bsrc.txt"
 with open(banner_file_path, "r") as banner_file:
   banner_text = banner_file.read()
 
-# Print
+# banner
 print(banner_text)
 
-# Get the path 
+# Get the path
 text_file_path = input("Enter the path to the text file: ")
 
-# Get the path where to save 
+# where to save
 public_uids_file_path = input("Enter the path where to save the public UIDs: ")
 
-# Create a list to store
+# store 
 public_uids = []
 
-# Iterate 
+# Iterate
 with open(text_file_path) as text_file:
   for line in text_file:
     uid = line.strip()
     is_public = is_public_uid(uid)
 
-    # Print and indicate 
+    # Print public or not public.
     if is_public:
       print(f"{uid} is public.")
       public_uids.append(uid)
     else:
       print(f"{uid} is not public.")
 
-# Save uid
+# Save UIDs
 with open(public_uids_file_path, "w") as public_uids_file:
   for uid in public_uids:
     public_uids_file.write(uid + "\n")
 
-#no public uid
+# no public UIDs
 if len(public_uids) == 0:
   print("Sorry man!! None of the uids was public.. try another file")
 else:
